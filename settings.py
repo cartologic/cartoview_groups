04186d0061a7eaaec9fa96b86e470f0089035ffc
hisham waleed karam <hisham.karam@cartologic.com>
2017-05-09 10:26:58 +0200

    Fix settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 # this file is excuted from cartoview.app_manager.settings using exec_file
 import os
 
-from apps import cartoview_groups
+import cartoview_groups
 
 app_folder = os.path.dirname(cartoview_groups.__file__)
 TEMPLATES[0]['DIRS'] = [os.path.join(app_folder, 'templates'), ] + TEMPLATES[0]['DIRS']
