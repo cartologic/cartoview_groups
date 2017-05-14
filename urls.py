ddad53a8ba9fdf46fee6a7ffe895f9917743b3fc
hisham waleed karam <hisham.karam@cartologic.com>
2017-05-14 09:40:24 +0200

    fix reverse error
    using reverse_lazy instead

--- urls.py
+++ urls.py
@@ -3,7 +3,4 @@ from django.conf.urls import patterns, url
 from . import views, APP_NAME
 
 urlpatterns = patterns('',
-                       # app urls
-   url(r'^$', views.index, name='%s.index' % APP_NAME),
-
-                       )
+                       url(r'^$', views.index, name='%s.index' % APP_NAME), )
--- views.py
+++ views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.decorators import login_required
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render, HttpResponse, redirect, HttpResponseRedirect
 # your views here
 from . import APP_NAME
 
 
-@login_required(login_url=reverse('account_login'))
+@login_required(login_url=reverse_lazy('account_login'))
 def index(request):
     return HttpResponseRedirect(reverse('group_detail', kwargs={'slug': request.user.group_list_all().first().slug}))
