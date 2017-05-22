6bdc2c930481518c8810c3d2715eec631e80a91e
hisham waleed karam <hisham.karam@cartologic.com>
2017-05-22 12:37:15 +0200

    Fixing reverse error

--- views.py
+++ views.py
@@ -7,4 +7,8 @@ from . import APP_NAME
 
 @login_required(login_url=reverse_lazy('account_login'))
 def index(request):
-    return HttpResponseRedirect(reverse('group_detail', kwargs={'slug': request.user.group_list_all().first().slug}))
+    if request.user.group_list_all():
+        return HttpResponseRedirect(
+            reverse_lazy('group_detail', kwargs={'slug': request.user.group_list_all().first().slug}))
+    else:
+        return HttpResponseRedirect(reverse_lazy('group_list'))
